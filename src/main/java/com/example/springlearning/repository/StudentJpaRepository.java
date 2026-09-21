2ba4ef359afd4113e24df7828e82ba0dcf5ceb33
Zubair Ali Bhutto <bhuttozubair6@gmail.com>
2026-09-21 17:26:12 +0500

    Tried teh basic concept of JPQL

--- src/main/java/com/example/springlearning/repository/StudentJpaRepository.java
+++ src/main/java/com/example/springlearning/repository/StudentJpaRepository.java
@@ -1,9 +1,16 @@
 package com.example.springlearning.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.example.springlearning.model.Student;
 
 public interface StudentJpaRepository extends JpaRepository<Student, Integer> {
+	
+	@Query("SELECT s FROM Student s WHERE s.name = :name")
+	List<Student> findStudentsByName(@Param("name") String name);
 
 }
