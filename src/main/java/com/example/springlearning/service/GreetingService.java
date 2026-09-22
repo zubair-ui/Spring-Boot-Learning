8a9d7c46045644e855d7a1cdd849fd6628c3258b
Zubair Ali Bhutto <bhuttozubair6@gmail.com>
2026-09-22 12:49:10 +0500

    Understanding the concept of Logging in SPring

--- src/main/java/com/example/springlearning/service/GreetingService.java
+++ src/main/java/com/example/springlearning/service/GreetingService.java
@@ -3,13 +3,23 @@ package com.example.springlearning.service;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 @Service
 public class GreetingService {
+	
+	private static final Logger logger =
+	        LoggerFactory.getLogger(GreetingService.class);
 
     @Value("${app.name:Default Application}")
     private String appName;
 
     public String getGreeting() {
+
+        logger.info("Getting greeting from GreetingService");
+        logger.debug("Preparing greeting response");
+
         return "Hello from GreetingService!";
     }
 
