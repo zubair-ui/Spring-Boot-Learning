f0582586cea10a6e910d29c739e680240d8eeca7
Zubair Ali Bhutto <bhuttozubair6@gmail.com>
2026-09-23 14:30:41 +0500

    Explicitly stating the JOIN table which is created by Hibernate

--- src/main/java/com/example/springlearning/model/Student.java
+++ src/main/java/com/example/springlearning/model/Student.java
@@ -7,6 +7,8 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToMany;
 import jakarta.persistence.OneToOne;
 
@@ -24,6 +26,11 @@ public class Student {
     private StudentProfile profile;
 
     @ManyToMany
+    @JoinTable(
+            name = "student_course",
+            joinColumns = @JoinColumn(name = "student_id"),
+            inverseJoinColumns = @JoinColumn(name = "course_id")
+    )
     private List<Course> courses = new ArrayList<>();
 
     public Student() {
